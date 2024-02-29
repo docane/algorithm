fa82a01c7b5e1c08895314fb647bf141af960ddd
docane <49787042+docane@users.noreply.github.com>
2024-02-29 13:55:06 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 304 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -3,68 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-int lower_bound(vector<int>& arr, int left, int right, int target){
-    while (left < right){
-        int mid = (left + right) / 2;
-        if (arr[mid] < target){
-            left = mid + 1;
-        }
-        else {
-            right = mid;
-        }
-    }
-    // printf("%d ", right);
-    return right;
-}
-
-int upper_bound(vector<int>& arr, int left, int right, int target){
-    while (left < right){
-        int mid = (left + right) / 2;
-        if (arr[mid] <= target){
-            left = mid + 1;
-        }
-        else {
-            right = mid;
-        }
-    }
-    // printf("%d ", right);
-    return right;
-}
-
-// void find(vector<int>& arr, int left, int right, int target, int* result){
-//     int center = (left + right) / 2;
-//     
-//     if (arr[center] == target){
-//         int lower = lower_bound(arr, left, right, target);
-//         int upper = upper_bound(arr, left, right, target);
-//         
-//         (*result) += upper - lower + 1;
-//         // while ((arr[center] == target) && (center > 0)){
-//         //     center--;
-//         //     if (arr[center] == target){
-//         //         (*result)++;
-//         //     }
-//         // }
-//         // center = (left + right) / 2;
-//         // while ((arr[center] == target) && (center < arr.size())){
-//         //     center++;
-//         //     if (arr[center] == target){
-//         //         (*result)++;
-//         //     }
-//         // }
-//         return;
-//     }
-//     else if (left > right){
-//         return;
-//     }
-//     else if (arr[center] > target){
-//         find(arr, left, center - 1, target, result);
-//     }
-//     else {
-//         find(arr, center + 1, right, target, result);
-//     }
-// }
-
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -79,17 +17,10 @@ int main(){
         have.push_back(temp);
     }
     sort(have.begin(), have.end());
-    // for (int i = 0; i < n; i++){
-    //     cout << have[i] << ' ';
-    // }
-    // cout << endl;
     cin >> m;
     for (int i = 0; i < m; i++){
         int target;
-        int* result = new int(0);
         cin >> target;
-        // find(have, 0, (int)have.size(), target, result);
-        //printf("%d\n", *result);
-        cout << upper_bound(have, 0, have.size(), target) - lower_bound(have, 0, have.size(), target) << ' ';
+        cout << upper_bound(have.begin(), have.end(), target) - lower_bound(have.begin(), have.end(), target) << ' ';
     }
 }
